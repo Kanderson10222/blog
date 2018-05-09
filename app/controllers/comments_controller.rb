2d8d6718e0902b93b573201cf9453a85acd7d488
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-09 12:12:05 -0400

    user, comments, and post link_to user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,20 @@ class CommentsController < ApplicationController
 
 def create
   @post = Post.find(params[:post_id])
-  @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+  @comment = @post.comments.new(params[:comment].permit(:name, :comment))
+  @comment.user_id = current_user.id
+  @comment.save
   redirect_to post_path(@post)
 end
 
 def destroy
   @post = Post.find(params[:post_id])
   @comment = @post.comments.find(params[:id])
+  if @comment.user_id == current_user.id
   @comment.destroy
+  end
   redirect_to post_path(@post)
+
 end
 
 
